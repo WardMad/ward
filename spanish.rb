599158a10cc2a5560cd475a2d6f298c33a227d87
ward <vakpko@gmail.com>
2017-11-01 16:12:06 +0100

    steps

--- spanish.rb
+++ spanish.rb
@@ -1,4 +1,4 @@
-def print_divider
+def print_divider  #cool method
   puts "*" * 40
   puts "\n"
 end
@@ -43,5 +43,86 @@ loop do
   else
     puts "That's not a valid input. Try again."
   end
+end
+
+steps = [
+  { description: "Scrape potatoes", action: "scrape_potatoes" },
+  { description: "Cut potatoes into thick slices", action: "cut_potatoes" },
+  { description: "Chop the onion", action: "generic_recipe_step" },
+  { description: "Heat the oil in a large frying pan", action: "generic_recipe_step" },
+  { description: "Add potatoes to the pan", action: "add_potatoes_to_pan" },
+  { description: "Add onion to the pan", action: "generic_recipe_step" },
+  { description: "Strain potatoes and onions through a colander into a large bowl", action: "generic_recipe_step" },
+  { description: "Break the eggs", action: "break_eggs" },
+  { description: "Beat the eggs separately", action: "generic_recipe_step" },
+  { description: "Stir into the bowl the potatoes with plenty of salt", action: "generic_recipe_step" },
+  { description: "Heat a little of the strained oil in a smaller pan", action: "generic_recipe_step" },
+  { description: "Tip everything into the pan and cook on a moderate heat", action: "generic_recipe_step"},
+  { description: "When almost set, invert on a plate and slide back into the pan", action: "generic_recipe_step" },
+  { description: "Cook a few more minutes and slide on to a plate and cool for 10 minutes before serving", action: "generic_recipe_step" }
+]
+
+puts "See below the different steps to follow:"
+print_progress_bar
+steps.each_with_index do |step, index|
+  puts (index + 1).to_s + ") " + step[:description]
+end
+def generic_recipe_step
+  puts "On it!"
+  print_progress_bar
+end
+
+def scrape_potatoes
+  counter = 0
+  while counter < NUM_POTATOES
+    counter += 1
+    print "Scraping potato #{counter}"
+    print_progress_bar
+  end
+end
+
+def cut_potatoes
+  counter = 0
+  while counter < NUM_POTATOES
+    counter += 1
+    print "Cutting potato #{counter} into thick slices"
+    print_progress_bar
+  end
+end
+
+def add_potatoes_to_pan
+  for counter in 1..NUM_POTATOES
+    print "Adding potato #{counter} to the pan."
+    print_progress_bar
+  end
+end
+
+def break_eggs
+  counter = 0
+  while counter < NUM_EGGS
+    counter += 1
+    print "Breaking egg #{counter}"
+    print_progress_bar
+  end
+end
+def ask_if_ready(step, index)
+  puts "Are you ready for step #{index + 1}?\n(#{step[:description]})"
+  answer = gets.chomp
+
+  answer.upcase == 'Y'
+end
+
+steps.each_with_index do |step, index|
+  print_divider
+
+  loop do
+    ready = ask_if_ready(step, index)
+    break if ready
+
+    puts "OK, I will give you some extra time."
+    print_progress_bar
+  end
+
+  send(step[:action])
 end
 print_divider
